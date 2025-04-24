f3430aeb3e95c48a6a9659ea4cc7a968b8c2f061
Carlos Torres <kisamehatake33@comunidad.unam.mx>
2025-04-24 10:57:50 -0600

    Init Config

---
+++ app/src/main/java/com/carlostorres/weatherapppositivo/ui/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.carlostorres.weatherapppositivo.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.carlostorres.weatherapppositivo.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+
+}
